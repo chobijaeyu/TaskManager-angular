0d583a3aa092d457ac1ba2dd8393d730ec431411
chobijaeyu <chobijaeyu@gmail.com>
2019-07-11 13:38:18 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'CardTask';
+}
