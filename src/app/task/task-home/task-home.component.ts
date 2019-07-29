4ef7a44dac5960dc5fe19b35dd7793334d136e9a
yujun <yujun@MacPro.local>
2019-07-29 16:55:53 +0900

    observable bug

--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, } from '@angular/core';
 import { ActivatedRoute, } from '@angular/router';
 import { ProjectDataService } from 'src/app/services/project-data.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'card-task-home',
@@ -9,7 +10,7 @@ import { ProjectDataService } from 'src/app/services/project-data.service';
 })
 export class TaskHomeComponent implements OnInit{
   ProjectData = {}
-  futurDate: Date
+  futurDate$: Observable<Date>
 
   TaskList = [{ Title: "チェック", Status: 1, data: [] }, { Title: "掃除", Status: 2, data: [] }, { Title: "梱包", Status: 3, data: [] }]
 
@@ -18,16 +19,13 @@ export class TaskHomeComponent implements OnInit{
     private taskDataService: ProjectDataService,
   ) {
     let ID = this.route.snapshot.paramMap.get("ID")
-    // when you can get Deadline from observiable
-    this.futurDate = new Date(this.route.snapshot.paramMap.get("Deadline"))
-    console.log(ID)
+    // when you can't get Deadline from observiable
+    // this.futurDate = new Date(this.route.snapshot.paramMap.get("Deadline"))
     this.taskDataService.GetOneProject(ID).subscribe(
       res => {
         this.ProjectData = res.Data
         console.log(this.ProjectData)
-        // this.futurDate = new Date(res.Data['Deadline'])
-        console.log(this.futurDate)
-        console.log(this.route.snapshot)
+        this.futurDate$ = of(new Date(res.Data['Deadline'])) 
         this.TaskList.forEach(el => {
           el.data = this.ProjectData['Tasklist'].filter(x => x["Status"] === el.Status)
         })
